084f5f2d51c6b51ccc1b394d2f328e04431b19df
Niclas Bassili <bassili@kth.se>
2023-04-19 21:30:02 +0200

    Added compensation for nonlinear ADC

--- src/battery.cpp
+++ src/battery.cpp
@@ -66,8 +66,7 @@ float BATTERY::readBatteryAndCalcValue(){
   #if BOARD_VERSION == 10
     float reading = 1.1 +  (readValue * (13.3 / ANALOG_RESOLUTION_MAX_VALUE));
   #elif BOARD_VERSION == 11
-    // float reading = 0.85 +  (readValue * (16.5 / ANALOG_RESOLUTION_MAX_VALUE));
-    float reading = (R_IN+R_GND)/R_GND * readValue * (3.3/ANALOG_RESOLUTION_MAX_VALUE);
+    float reading = (R_IN+R_GND)/R_GND * (readValue * (ADC_LINEAR_MAX_V-ADC_DETECTION_V)/ADC_LINEAR_MAX_READING + ADC_DETECTION_V);
   #endif
   return reading;
 }
--- src/battery.h
+++ src/battery.h
@@ -15,8 +15,8 @@
 #include "utils.h"
 
 // Voltages should be specified in V
-#define BATTERY_FULL       15.000 //TODO: Change when changing to new battery!
-#define BATTERY_EMPTY      13.600 //TODO: Change when changing to new battery!
+#define BATTERY_FULL       12.560 //TODO: Change according to actual battery used!
+#define BATTERY_EMPTY      10.400 //TODO: Change according to actual battery used!
 
 // Running average sample size
 #define FILTER        2000
@@ -24,6 +24,12 @@
 // Voltage divider resistors (Same for both battery and charge connections)
 #define R_IN    330000  // Resistance in ohm of resistor connected between battery+ and analog input pin.
 #define R_GND   82000   // Resistance in ohm of resistor connected between GND and analog input pin.
+
+// ADC Calibrations (Used for rough compensation for the nonlinear and not factory calibrated adc in ESP32)
+#define ADC_SATURATION_V 3.195 // Not used in calculations, but good to have for future reference
+#define ADC_LINEAR_MAX_V 2.75 // Voltage on gpio when readings starts to become exponential
+#define ADC_LINEAR_MAX_READING 1630
+#define ADC_DETECTION_V 0.21
 
 class BATTERY {
   public:
